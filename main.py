6c387abb0545953160ab0780628ebe87791afbb8
Pietro Zarri <pietrozarri96@gmail.com>
2022-07-08 17:00:46 +0200

    improved cl args

--- main.py
+++ main.py
@@ -62,26 +62,35 @@ def hmde(points, size, max_iteration, n_clusters, matching_type, do_verbose):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Memetic Differential Evolution (MDE) for clustering")
-    # parser.add_argument("-d", type=str, help="dataset filename")
+    parser.add_argument('-method',
+                        default='MDE',
+                        choices=['MDE', 'GMDE', 'DMDE', 'HMDE'],
+                        help='MDE variant')
     parser.add_argument("-s", type=int, help="size of the population")
     parser.add_argument("-i", type=int, help="number of allowed consecutive iterations without improvements of the"
                                              " best solution")
+
     parser.add_argument('-matching',
                         default='exact',
                         choices=['exact', 'greedy'],
                         help='matching algorithm')
     parser.add_argument("-m", help="execute mutation", action="store_true")
+    parser.add_argument("--d", type=str, help="dataset filename")
     parser.add_argument("-v", "--verbose", help="verbose output", action="store_true")
 
     args = parser.parse_args()
-    # dataset = args.d
+    method = args.method
+    if args.d is not None:
+        datasets = [args.d]
+    else:
+        datasets = ['ionosphere.txt', 'page.txt', 'pendigit.txt']
     population_size = args.s
     max_iteration = args.i
     verbose = args.verbose
     mutation = args.m
     matching = args.matching
 
-    for dataset in ['ionosphere.txt', 'page.txt', 'pendigit.txt']:
+    for dataset in datasets:
         dataset_path = os.path.join('points', dataset)
         if not os.path.exists(dataset_path):
             raise ValueError(f'Can\'t find dataset at the following path: {dataset_path}')
@@ -90,9 +99,8 @@ if __name__ == '__main__':
         print('#########')
         points = np.genfromtxt(dataset_path)
 
-
-        # if population_size < 4:
-        #    raise argparse.ArgumentTypeError('Population size can\'t be less than 3')
+        if population_size < 4:
+            raise argparse.ArgumentTypeError('Population size can\'t be less than 3')
 
         for seed in [16007, 10000, 12345, 00000, 56789]:
             np.random.seed(seed)
@@ -104,17 +112,37 @@ if __name__ == '__main__':
                 print('CLUSTER:', cluster_seq)
 
                 start_time = time.time()
-                # solution = mde(points=points,
-                #                size=population_size,
-                #                max_interation=max_iteration,
-                #                n_clusters=cluster_seq,
-                #                do_mutation=mutation,
-                #                matching_type=matching,
-                #                do_verbose=verbose)
 
-                solution = gmde(points=points, size=population_size, max_iteration=max_iteration, n_clusters=cluster_seq, matching_type=matching, do_verbose=verbose)
-                # solution = dmde(points=points, size=population_size, max_iteration=max_iteration, n_clusters=cluster_seq, matching_type=matching, do_verbose=verbose)
-                # solution = hmde(points=points, size=population_size, max_iteration=max_iteration, n_clusters=cluster_seq, matching_type=matching, do_verbose=verbose)
+                if method == 'MDE':
+                    solution = mde(points=points,
+                                   size=population_size,
+                                   max_interation=max_iteration,
+                                   n_clusters=cluster_seq,
+                                   do_mutation=mutation,
+                                   matching_type=matching,
+                                   do_verbose=verbose)
+                elif method == 'GMDE':
+                    solution = gmde(points=points,
+                                    size=population_size,
+                                    max_iteration=max_iteration,
+                                    n_clusters=cluster_seq,
+                                    matching_type=matching,
+                                    do_verbose=verbose)
+                elif method == 'DMDE':
+                    solution = dmde(points=points,
+                                    size=population_size,
+                                    max_iteration=max_iteration,
+                                    n_clusters=cluster_seq,
+                                    matching_type=matching,
+                                    do_verbose=verbose)
+                elif method == 'HMDE':
+                    solution = hmde(points=points,
+                                    size=population_size,
+                                    max_iteration=max_iteration,
+                                    n_clusters=cluster_seq,
+                                    matching_type=matching,
+                                    do_verbose=verbose)
+
                 execution_time = time.time() - start_time
 
                 utils.print_result(round(solution.get_score(), 4), KMeans.get_score(), round(execution_time, 2))
